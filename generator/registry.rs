36f71cba5dbb13ef4a275feca49a164e625ebb78
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2013-08-15 00:50:28 +1000

    Parse command prototypes

--- generator/registry.rs
+++ generator/registry.rs
@@ -16,13 +16,13 @@ impl Registry {
 
 pub struct EnumNs {
     namespace: ~str,
-    enums: ~[Enum],
     group: Option<~str>,
     ty: Option<~str>,
     start: Option<~str>,
     end: Option<~str>,
     vendor: Option<~str>,
     comment: Option<~str>,
+    enums: ~[Enum],
 }
 
 pub struct Enum {
@@ -30,22 +30,19 @@ pub struct Enum {
     value: ~str,
 }
 
-pub struct ReturnType(Option<~str>);
-
 pub struct CmdNs {
     namespace: ~str,
     cmds: ~[Cmd],
 }
 
-pub struct Cmd {
+pub struct Binding {
     ident: ~str,
-    ty: ReturnType,
-    params: ~[Param]
+    ty: ~str,
 }
 
-pub struct Param {
-    ptype: ~str,
-    ident: ~str,
+pub struct Cmd {
+    proto: Binding,
+    params: ~[Binding],
 }
 
 struct RegistryBuilder {
@@ -54,23 +51,42 @@ struct RegistryBuilder {
 
 impl<'self> RegistryBuilder {
     fn recv(&self) -> ParseEvent {
-        match self.port.recv() {
-            Ok(msg) => msg,
-            Err(err) => fail!("XML error: %s", err.to_str())
+        loop {
+            match self.port.recv() {
+                Ok(StartDocument) => (),
+                Ok(Comment(_)) => (),
+                Ok(Characters(ref ch)) if ch.is_whitespace() => (),
+                Ok(EndDocument) => fail!("The end of the document has been reached"),
+                Ok(event) => return event,
+                Err(err) => fail!("XML error: %s", err.to_str()),
+            }
         }
     }
 
     fn parse(data: &str) -> Registry {
-        let builder = RegistryBuilder {
+        RegistryBuilder {
             port: parse_xml(data)
-        };
-        match builder.recv() {
-            StartDocument => (),
-            msg => fail!("Expected start of document, found: %?", msg.to_str()),
+        }.consume_registry()
+    }
+
+    fn expect_characters(&self) -> ~str {
+        match self.recv() {
+            Characters(ref ch) => ch.clone(),
+            msg => fail!("Expected characters, found: %?", msg.to_str()),
         }
-        match builder.recv() {
-            StartElement(~"registry", _) => builder.consume_registry(),
-            msg => fail!("Expected <registry>, found: %?", msg.to_str()),
+    }
+
+    fn expect_start_element(&self, name: &str) -> Attributes {
+        match self.recv() {
+            StartElement(ref n, ref atts) if name == *n => atts.clone(),
+            msg => fail!("Expected <%s>, found: %?", name, msg.to_str()),
+        }
+    }
+
+    fn expect_end_element(&self, name: &str) {
+        match self.recv() {
+            EndElement(ref n) if name == *n => (),
+            msg => fail!("Expected </%s>, found: %?", name, msg.to_str()),
         }
     }
 
@@ -82,11 +98,12 @@ impl<'self> RegistryBuilder {
     }
 
     fn consume_registry(&self) -> Registry {
+        self.expect_start_element("registry");
         let mut registry = Registry { enum_nss: ~[], cmd_nss: ~[] };
         loop {
             match self.recv() {
                 // ignores
-                Characters(_) | Comment(_) | IgnorableWhitespace(_) => (),
+                Characters(_) | Comment(_) => (),
                 StartElement(~"comment", _) => self.skip_until(EndElement(~"comment")),
                 StartElement(~"types", _) => self.skip_until(EndElement(~"types")),
                 StartElement(~"groups", _) => self.skip_until(EndElement(~"groups")),
@@ -98,18 +115,26 @@ impl<'self> RegistryBuilder {
                     registry.enum_nss.push(
                         EnumNs {
                             namespace:  atts.get_copy(&~"namespace"),
-                            enums:      self.consume_enums(),
                             group:      atts.find_copy(&~"group"),
                             ty:         atts.find_copy(&~"type"),
                             start:      atts.find_copy(&~"start"),
                             end:        atts.find_copy(&~"end"),
                             vendor:     atts.find_copy(&~"vendor"),
                             comment:    atts.find_copy(&~"comment"),
+                            enums:      self.consume_enums(),
                         }
                     )
                 }
+
                 // add command namespace
-                StartElement(~"commands", _) => self.skip_until(EndElement(~"commands")),
+                StartElement(~"commands", ref atts) => {
+                    registry.cmd_nss.push(
+                        CmdNs {
+                            namespace:  atts.get_copy(&~"namespace"),
+                            cmds:       self.consume_cmds(),
+                        }
+                    );
+                }
 
                 // finished building the registry
                 EndElement(~"registry") => break,
@@ -126,8 +151,9 @@ impl<'self> RegistryBuilder {
         loop {
             match self.recv() {
                 // ignores
-                Characters(_) | Comment(_) | IgnorableWhitespace(_) => (),
+                Characters(_) | Comment(_) => (),
                 StartElement(~"unused", _) => self.skip_until(EndElement(~"unused")),
+
                 // add enum definition
                 StartElement(~"enum", ref atts) => {
                     enums.push(
@@ -136,11 +162,9 @@ impl<'self> RegistryBuilder {
                             value:  atts.get_copy(&~"value"),
                         }
                     );
-                    match self.recv() {
-                        EndElement(~"enum") => (),
-                        msg => fail!("Expected </enum>, found: %?", msg.to_str()),
-                    }
+                    self.expect_end_element("enum");
                 }
+
                 // finished building the namespace
                 EndElement(~"enums") => break,
                 // error handling
@@ -150,5 +174,52 @@ impl<'self> RegistryBuilder {
         enums
     }
 
-    // TODO: Consume command namespaces
+    fn consume_cmds(&self) -> ~[Cmd] {
+        let mut cmds = ~[];
+        loop {
+            match self.recv() {
+                // add command definition
+                StartElement(~"command", _) => {
+                    self.expect_start_element("proto");
+                    let proto = self.consume_binding();
+                    self.expect_end_element("proto");
+
+                    cmds.push(Cmd {
+                        proto: proto,
+                        params: ~[],
+                    });
+
+                    self.skip_until(EndElement(~"command"));
+                }
+                // finished building the namespace
+                EndElement(~"commands") => break,
+                // // error handling
+                msg => fail!("Expected </commands>, found: %?", msg.to_str()),
+            }
+        }
+        cmds
+    }
+
+    fn consume_binding(&self) -> Binding {
+        // consume type
+        let mut ty = ~"";
+        loop {
+            match self.recv() {
+                Characters(ch) => ty.push_str(ch),
+                StartElement(~"ptype", _) => (),
+                EndElement(~"ptype") => (),
+                StartElement(~"name", _) => break,
+                msg => fail!("Expected binding, found: %?", msg.to_str()),
+            }
+        }
+        let ident = self.expect_characters();
+        // consume identifier
+        self.expect_end_element("name");
+        Binding {
+            ident: ident,
+            ty: ty,
+        }
+    }
+
+    // TODO: Consume command parameters
 }
