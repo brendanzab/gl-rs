e14d45e79eb25a2bc6f37d740c8d042447bdd14d
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2013-08-17 00:38:31 +1000

    Begin implementing loader generation functions

--- generator/generator.rs
+++ generator/generator.rs
@@ -18,7 +18,7 @@ use std::io;
 use std::os;
 use std::util;
 
-use registry::Registry;
+use registry::*;
 
 pub mod registry;
 pub mod ty;
@@ -77,6 +77,52 @@ fn print_ctys(reg: &Registry) {
 fn print_rtys(reg: &Registry) {
     let tys = reg.get_tys();
     for ty in tys.iter() {
-        printfln!("\"%s\"", ty::from_cty(*ty));
+        printfln!("\"%s\"", ty::to_rust_ty(*ty));
     }
+}
+
+pub fn gen_binding_str(binding: &Binding) -> ~str {
+    fmt!("%s: %s", binding.ident, ty::to_rust_ty(binding.ty))
+}
+
+pub fn gen_param_list_str(cmd: &Cmd) -> ~str {
+    cmd.params.iter()
+        .map(|b| gen_binding_str(b))
+        .to_owned_vec()
+        .connect(", ")
+}
+
+pub fn gen_return_suffix_str(cmd: &Cmd) -> ~str {
+    ty::to_return_suffix(
+        ty::to_rust_ty(cmd.proto.ty)
+    )
+}
+
+pub mod gen_ptr {
+    use super::*;
+    use registry::*;
+
+    pub fn gen_static_mut_str(cmd: &Cmd) -> ~str {
+        fmt!("pub static mut %s: extern \"C\" fn(%s)%s = FAIL_%s;",
+            cmd.proto.ident,
+            gen_param_list_str(cmd),
+            gen_return_suffix_str(cmd),
+            cmd.proto.ident)
+    }
+
+    pub fn gen_failing_fn(cmd: &Cmd) -> ~str {
+        fmt!("extern fn \"C\" FAIL_%s(%s)%s { fail!(\"%s was not loaded\") }",
+            cmd.proto.ident,
+            gen_param_list_str(cmd),
+            gen_return_suffix_str(cmd),
+            cmd.proto.ident)
+    }
+
+    pub fn gen_enum_str(enm: &Enum, ty: &str) -> ~str {
+        fmt!("pub static %s: %s = %s;", enm.ident, ty, enm.value)
+    }
+}
+
+pub mod gen_struct {
+
 }
