bf334ee68f26b5bb5c63d06bc84a978f6130ee82
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2013-08-23 12:54:49 +1000

    Add a really basic example

---
+++ examples/ptr_example.rs
@@ -0,0 +1,39 @@
+extern mod glfw;
+extern mod gl;
+
+#[start]
+fn start(argc: int, argv: **u8, crate_map: *u8) -> int {
+    // Run GLFW on the main thread
+    std::rt::start_on_main_thread(argc, argv, crate_map, main)
+}
+
+fn main() {
+    do glfw::set_error_callback |_, description| {
+        printfln!("GLFW Error: %s", description);
+    }
+
+    do glfw::start {
+        // Choose a GL profile that is compatible with OS X 10.7+
+        glfw::window_hint::context_version(3, 2);
+        glfw::window_hint::opengl_profile(glfw::OPENGL_CORE_PROFILE);
+        glfw::window_hint::opengl_forward_compat(true);
+
+        let window = glfw::Window::create(800, 600, "OpenGL", glfw::Windowed).unwrap();
+        window.make_context_current();
+
+        // Load the OpenGL function pointers
+        gl::load_with(glfw::get_proc_address);
+
+        while !window.should_close() {
+            // Poll events
+            glfw::poll_events();
+
+            // Clear the screen to black
+            gl::ClearColor(0.3, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            // Swap buffers
+            window.swap_buffers();
+        }
+    }
+}
