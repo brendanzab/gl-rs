d0869dac3e2c5791a3c2458e65283e8dded0f7c5
ozkriff <ozkriff@gmail.com>
2014-06-29 20:30:24 +0400

    Updated fo Rust 0dfc90a: *const, *mut

--- src/examples/triangle.rs
+++ src/examples/triangle.rs
@@ -48,7 +48,7 @@ static FS_SRC: &'static str =
     }";
 
 #[start]
-fn start(argc: int, argv: **u8) -> int {
+fn start(argc: int, argv: *const *const u8) -> int {
     native::start(argc, argv, main)
 }
 
