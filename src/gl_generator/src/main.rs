3d44d6374adb5c90761f5ffcee2488e8b056fff5
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2014-07-30 13:36:24 +1000

    Add missing licence information and update year

--- src/gl_generator/src/main.rs
+++ src/gl_generator/src/main.rs
@@ -1,4 +1,4 @@
-// Copyright 2013 The gl-rs developers. For a full listing of the authors,
+// Copyright 2013-2014 The gl-rs developers. For a full listing of the authors,
 // refer to the AUTHORS file at the top-level directory of this distribution.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
