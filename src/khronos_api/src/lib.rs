865e56fd59408849579be4db2b79af19bfaca2bd
Tomaka17 <pierre.krieger1708@gmail.com>
2014-11-12 18:29:57 +0100

    Update for crates.io

--- src/khronos_api/src/lib.rs
+++ src/khronos_api/src/lib.rs
@@ -1,13 +1,13 @@
 //! This crates contains the sources of the official OpenGL repository.
 
 /// Content of the official `gl.xml` file.
-pub static GL_XML: &'static [u8] = include_bin!("../../../deps/khronos-api/gl.xml");
+pub static GL_XML: &'static [u8] = include_bin!("../api/gl.xml");
 
 /// Content of the official `egl.xml` file.
-pub static EGL_XML: &'static [u8] = include_bin!("../../../deps/khronos-api/egl.xml");
+pub static EGL_XML: &'static [u8] = include_bin!("../api/egl.xml");
 
 /// Content of the official `wgl.xml` file.
-pub static WGL_XML: &'static [u8] = include_bin!("../../../deps/khronos-api/wgl.xml");
+pub static WGL_XML: &'static [u8] = include_bin!("../api/wgl.xml");
 
 /// Content of the official `glx.xml` file.
-pub static GLX_XML: &'static [u8] = include_bin!("../../../deps/khronos-api/glx.xml");
+pub static GLX_XML: &'static [u8] = include_bin!("../api/glx.xml");
