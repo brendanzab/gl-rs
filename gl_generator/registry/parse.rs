a7ebdb0eab8894ed77a8471d18c70eab931e5ee3
Bastien Orivel <eijebong@bananium.fr>
2019-08-15 17:53:44 +0200

    Fix the parser to accomodate for a typo in khronos_api/api_angle and bump version
    Fixes #491

--- gl_generator/registry/parse.rs
+++ gl_generator/registry/parse.rs
@@ -1041,6 +1041,7 @@ pub fn to_rust_ty<T: AsRef<str>>(ty: T) -> Cow<'static, str> {
         "EGLAttrib" => "types::EGLAttrib",
         "EGLAttrib *" => "*mut types::EGLAttrib",
         "const EGLAttrib *" => "*const types::EGLAttrib",
+        "const EGLattrib *" => "*const types::EGLAttrib", // Due to a typo in khronos_api/api_angle/scripts/egl_angle_ext.xml - see brendanzab/gl-rs#491
         "EGLConfig" => "types::EGLConfig",
         "EGLConfig *" => "*mut types::EGLConfig",
         "EGLContext" => "types::EGLContext",
