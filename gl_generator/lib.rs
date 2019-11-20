afadaaedc0531b93ca79dce1a54210c62dcd0742
Lokathor <zefria@gmail.com>
2019-11-19 19:01:13 -0700

    restore license messages.

--- gl_generator/lib.rs
+++ gl_generator/lib.rs
@@ -1,6 +1,16 @@
-// Based on the `gl_generator` crate, by Brendan Zabarauskas and the gl-rs
-// developers (2015). Used under Apache-2.0 license. All new content is also
-// available under the Apache-2.0 license.
+// Copyright 2015 Brendan Zabarauskas and the gl-rs developers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 //! This is a fork of [gl_generator](https://docs.rs/gl_generator).
 //!
